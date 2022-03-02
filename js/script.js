193d3afc568f6e6a62fb899f0ff3bcce1d9ae479
crystal_c <crystalc.dev@gmail.com>
2022-03-02 16:38:35 -0600

    Added function to validate input, added array for guesses and function to collect guesses, and updated event handler

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const guessed = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
-const letterEntered = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 const wordProgress = document.querySelector(".word-in-progress");
 const guessRemaining = document.querySelector(".remaining");
 const guessCount = document.querySelector(".remaining span");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeCircles = function (word) {
     const letterArray = [];
@@ -20,9 +21,44 @@ const placeCircles = function (word) {
 
 placeCircles(word);
 
+
+const checkInput = function (guess) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    if (guess === "") {
+        message.innerText = "Please enter a guess.";
+    } else if (guess.length > 1) {
+        message.innerText = "Please guess one letter.";
+    } else if (!guess.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter.";
+    } else {
+        message.innerText = `${guess.toUpperCase()}`;
+        return guess;
+    };
+};
+
+const makeGuess = function(goodGuess) {
+    goodGuess.toUpperCase();
+
+    if (guessedLetters.includes(goodGuess)) {
+        message.innerText = `You've already guessed ${goodGuess}. Please try again.`;
+    } else {
+        guessedLetters.push(goodGuess);
+    }
+    console.log(guessedLetters);
+};
+
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const guess = letterEntered.value;
-    // console.log(guess);
-    letterEntered.value = "";
+    const guess = letterInput.value;
+
+    message.innerText = "";
+
+    const goodGuess = checkInput(guess);
+
+    if (goodGuess) {
+        makeGuess(goodGuess);
+    };
+
+    letterInput.value = "";
 });
